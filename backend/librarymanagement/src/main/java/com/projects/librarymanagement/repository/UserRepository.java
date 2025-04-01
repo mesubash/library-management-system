b270c641bc87d4787b35c9033718c48d4869f082
mesubash <subashdhamee@gmail.com>
2025-04-01 22:28:02 +0545

    user repo added

---
+++ backend/librarymanagement/src/main/java/com/projects/librarymanagement/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.projects.librarymanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.projects.librarymanagement.model.User;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    User findByEmail(String email);
+    User findByPhoneNumber(String phoneNumber);
+    User findByAddress(String address);
+    User findByRole(String role);
+    
+}
