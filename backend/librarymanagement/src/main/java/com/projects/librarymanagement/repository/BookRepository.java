6a39c8ec7de5b9da6641da054ef67c7fffada6e6
mesubash <subashdhamee@gmail.com>
2025-04-01 22:27:02 +0545

    book repo structure added

---
+++ backend/librarymanagement/src/main/java/com/projects/librarymanagement/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.projects.librarymanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.projects.librarymanagement.model.Book;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long>{
+    
+}
