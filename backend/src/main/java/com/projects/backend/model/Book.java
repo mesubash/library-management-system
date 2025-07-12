4c7cbb4e0a0ed5ac5f52ee24d2bce0d80aaf2a2f
mesubash <subashdhamee@gmail.com>
2025-07-12 17:59:08 +0545

    update book model

--- backend/src/main/java/com/projects/backend/model/Book.java
+++ backend/src/main/java/com/projects/backend/model/Book.java
@@ -1,41 +1,125 @@
 package com.projects.backend.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;    
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "books")
-@Data
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Title is required")
     private String title;
+
+    @NotBlank(message = "Author is required")
     private String author;
+
+    @NotBlank(message = "ISBN is required")
     private String isbn;
-    private String publisher;
-    private int yearPublished;
+
     private String genre;
-    private int numberOfPages;
-    private String language;
-    private String description;
-    // private String coverImageUrl;
-    private boolean isAvailable;
-    // private int numberOfCopies;
-    // private String location; // e.g., shelf number or library branch
-    // private String addedBy; // User who added the book
-    // private String addedDate; // Date when the book was added to the system
-    // private String lastUpdatedBy; // User who last updated the book details
-    // private String lastUpdatedDate; // Date when the book details were last updated
-    // private String status; // e.g., available, checked out, reserved, lost
 
-    
-  
+    @NotNull(message = "Published year is required")
+    private Integer publishedYear;
 
+    @NotNull(message = "Total copies is required")
+    private Integer totalCopies;
 
-    
+    private Integer availableCopies;
+
+    private Long categoryId;
+
+    public Book() {}
+
+    public Book(Long id, String title, String author, String isbn, String genre, Integer publishedYear,
+                Integer totalCopies, Integer availableCopies, Long categoryId) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.genre = genre;
+        this.publishedYear = publishedYear;
+        this.totalCopies = totalCopies;
+        this.availableCopies = availableCopies;
+        this.categoryId = categoryId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Integer getPublishedYear() {
+        return publishedYear;
+    }
+
+    public void setPublishedYear(Integer publishedYear) {
+        this.publishedYear = publishedYear;
+    }
+
+    public Integer getTotalCopies() {
+        return totalCopies;
+    }
+
+    public void setTotalCopies(Integer totalCopies) {
+        this.totalCopies = totalCopies;
+    }
+
+    public Integer getAvailableCopies() {
+        return availableCopies;
+    }
+
+    public void setAvailableCopies(Integer availableCopies) {
+        this.availableCopies = availableCopies;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
 }
