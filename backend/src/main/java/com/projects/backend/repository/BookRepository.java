6cdc441c3e6d5f91177c50966cf710da92661a8a
mesubash <subashdhamee@gmail.com>
2025-07-12 18:00:02 +0545

    update book repo

--- backend/src/main/java/com/projects/backend/repository/BookRepository.java
+++ backend/src/main/java/com/projects/backend/repository/BookRepository.java
@@ -1,11 +1,19 @@
 package com.projects.backend.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.projects.backend.model.Book;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, Long>{
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByTitleContainingIgnoreCaseOrAuthorContainingIgnoreCaseOrIsbnContainingIgnoreCase(
+            String title, String author, String isbn);
     
+    List<Book> findByAvailableCopiesGreaterThan(Integer copies);
+    
+    List<Book> findByCategoryId(Long categoryId);
+    
+    List<Book> findByGenreIgnoreCase(String genre);
 }
