e6bde78662f66a601236a7c7e413c1ca629ba6cc
mesubash <subashdhamee@gmail.com>
2025-07-12 18:00:41 +0545

    update user repo

--- backend/src/main/java/com/projects/backend/repository/UserRepository.java
+++ backend/src/main/java/com/projects/backend/repository/UserRepository.java
@@ -1,17 +1,13 @@
 package com.projects.backend.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.projects.backend.model.User;
 
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-    User findByEmail(String email);
-    User findByPhoneNumber(String phoneNumber);
-    User findByAddress(String address);
-    User findByRole(String role);
-    
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
